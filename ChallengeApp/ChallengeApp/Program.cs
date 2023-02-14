728bbd1cdd0d049d99e3f58f9e28ffc65a56a1e6
Magorzata Cieślak <malgorzatacieslak8509@gmail.com>
2023-02-14 15:14:42 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Dzień 4
+var name = "Ewa";
+bool female = true;
+var age = 33;
+
+if (female && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Ktoś inny");
+}
